6f96df64f18e1ee089e08c7adadf111e0eeea94e
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2023-07-28 20:47:27 +0530

    Add files via upload

---
+++ task 03 - MidKnight Studio clone/script.js
@@ -0,0 +1,37 @@
+//code for loader
+window.addEventListener("load",function(){
+    var loader=document.querySelector(".loader");
+    loader.style.display="none";
+})
+//code for navigation bar color
+window.addEventListener("scroll",function(){
+    var nav=document.querySelector("nav");
+    if(window.scrollY>40){
+        nav.style.backgroundColor="black";
+    }
+    else{
+        nav.style.backgroundColor="transparent";
+    }
+}); 
+//code for title animation 
+document.addEventListener("DOMContentLoaded",function(){
+    var title=document.querySelector(".title h1");
+    function showTitle(){
+        // title.classList.remove("hidden");
+        title.style.visibility="visible";
+        title.classList.add("visible");
+    }
+    setTimeout(showTitle,1500);
+});
+// code for slider
+let scroll=document.querySelector(".gallery");
+let backbtn=document.querySelector(".backbtn");
+let nextbtn=document.querySelector(".nextbtn");
+scroll.style.scrollBehavior="smooth";
+
+nextbtn.addEventListener("click",function(){
+    scroll.scrollLeft+=2000;
+});
+backbtn.addEventListener("click",function(){
+    scroll.scrollLeft-=2000;
+});
