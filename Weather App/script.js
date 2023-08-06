005d68be40efc17a469d6e6a0a372c31dbd02936
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2023-08-06 19:28:11 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,4 @@
+document.querySelector(".search_box i").addEventListener("click",function(){
+    document.querySelector(".content").style.display='flex';
+    document.querySelector(".extras").style.display='flex';
+});
