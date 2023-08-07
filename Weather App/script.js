6ab7f34a623a04fc0fbe7acaa2608597bfb9be71
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2023-08-07 18:32:13 +0530

    Delete Weather App directory

--- Weather App/script.js
+++
@@ -1,4 +0,0 @@
-document.querySelector(".search_box i").addEventListener("click",function(){
-    document.querySelector(".content").style.display='flex';
-    document.querySelector(".extras").style.display='flex';
-});
