a25773342432c7a3478662b3d5c03089d8877194
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2023-08-07 18:33:03 +0530

    Add files via upload

---
+++ Weather App/script.js
@@ -0,0 +1,76 @@
+function main(){
+    if(document.querySelector(".search_box input").value==''){
+        alert("No input given");
+    }
+    else{
+        fetching();
+        document.querySelector(".content").style.display='flex';
+        document.querySelector(".extras").style.display='flex';
+    }
+}
+
+function fetching(){
+    const apiUrl1='https://api.openweathermap.org/data/2.5/weather?q=';
+    const apiUrl2='&appid=7a230a2594131583dcd6abfdd7bf2443&units=metric';
+    let city=document.querySelector(".search_box input").value;
+    fetch(apiUrl1+city+apiUrl2)
+    .then(function(response){
+        return response.json();
+    })
+    .then(function(data){
+        document.querySelector("#city").style.fontSize='35px';
+        document.querySelector("#city").innerHTML=data.name;
+        document.querySelector("#temperature").innerHTML=Math.ceil(data.main.temp)+'&deg;c';
+        document.querySelector(".wind h2").innerHTML=data.wind.speed+'Km/h';
+        document.querySelector(".humidity h2").innerHTML=data.main.humidity+'%';
+        console.log(data);
+        weather(data);
+    })
+    .catch(function(){
+        document.querySelector(".content img").style.width='75px';
+        document.querySelector(".content img").setAttribute("src",'https://images.emojiterra.com/google/android-11/512px/1f972.png');
+        document.querySelector("#temperature").innerHTML='';
+        document.querySelector("#city").style.fontSize='25px';
+        document.querySelector("#city").innerHTML='City Not Found';
+        document.querySelector(".wind h2").innerHTML='';
+        document.querySelector(".humidity h2").innerHTML='';
+        alert("plz enter correct city name");
+    })
+}
+
+function weather(data){
+    document.querySelector(".content img").style.width='110px';
+    switch(data.weather[0].main){
+        case 'Haze':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/mist.png');
+            break;
+        case 'Clouds':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clouds.png');
+            break;
+        case 'Snow':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/snow.png');                
+            break;
+        case 'Clear':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clear.png');
+            break;
+        case 'Rain':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/rain.png');
+            break;      
+        case 'Drizzle':
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/drizzle.png');
+            break;
+        default:
+            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clouds.png');
+            break;
+    }
+}
+
+document.querySelector(".search_box i").addEventListener("click",function(){
+    main();
+});
+
+document.addEventListener("keydown",function(event){
+    if(event.key=='Enter'){
+        main();
+    }
+});
