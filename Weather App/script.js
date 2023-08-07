627fb61ea51de5f79f7788cc2c1bf7ba3c76f89c
Suyash Deshpande <97775667+iSuyash69@users.noreply.github.com>
2023-08-07 18:37:26 +0530

    Update script.js

--- Weather App/script.js
+++ Weather App/script.js
@@ -42,25 +42,25 @@ function weather(data){
     document.querySelector(".content img").style.width='110px';
     switch(data.weather[0].main){
         case 'Haze':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/mist.png');
+            document.querySelector(".content img").setAttribute("src",'images/mist.png');
             break;
         case 'Clouds':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clouds.png');
+            document.querySelector(".content img").setAttribute("src",'images/clouds.png');
             break;
         case 'Snow':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/snow.png');                
+            document.querySelector(".content img").setAttribute("src",'images/snow.png');                
             break;
         case 'Clear':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clear.png');
+            document.querySelector(".content img").setAttribute("src",'C:images/clear.png');
             break;
         case 'Rain':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/rain.png');
+            document.querySelector(".content img").setAttribute("src",'images/rain.png');
             break;      
         case 'Drizzle':
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/drizzle.png');
+            document.querySelector(".content img").setAttribute("src",'images/drizzle.png');
             break;
         default:
-            document.querySelector(".content img").setAttribute("src",'C:/Users/ACER/Downloads/iSuyash-webDev-main/iSuyash-webDev-main/Weather App/images/clouds.png');
+            document.querySelector(".content img").setAttribute("src",'images/clouds.png');
             break;
     }
 }
